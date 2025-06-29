dfce09fd5f54270904b06685bfcfa5c0a4457d3f
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-29 16:57:49 +0300

    Add Spring COntext and WEbMvc config

---
+++ src/main/java/org/example/app/config/AppConfig.java
@@ -0,0 +1,6 @@
+package org.example.app.config;
+
+
+
+public class AppConfig {
+}
---
+++ src/main/java/org/example/app/config/WebAppConfig.java
@@ -0,0 +1,12 @@
+package org.example.app.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+
+public class WebAppConfig implements WebMvcConfigurer {
+}
