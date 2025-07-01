79a9d427511fe11d2407d00b9b653fc4052bab31
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-01 13:32:00 +0300

    Add @JsonProperty names for each attribute in EmployeeDtoRequest

--- src/main/java/org/example/app/dto/EmployeeDtoRequest.java
+++ src/main/java/org/example/app/dto/EmployeeDtoRequest.java
@@ -2,13 +2,22 @@ package org.example.app.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record EmployeeDtoRequest(
         Long id,
+
+        @JsonProperty("first_name")
         String firstName,
+
+        @JsonProperty("last_name")
         String lastName,
+
+        @JsonProperty("position_id")
         Long positionId,
+
+        @JsonProperty("phone")
         String phone
 ) {
 }
